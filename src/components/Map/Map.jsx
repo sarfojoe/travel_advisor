45d9ae775f94115b92fbddb9e8230854ed3a9b56
sarfojoe <60213446+sarfojoe@users.noreply.github.com>
2021-08-29 23:45:41 +0300

    style added

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,10 +2,9 @@ import React from "react";
 import GoogleMapReact from "google-map-react";
 import { Paper, Typography, useMediaQuery } from "@material-ui/core";
 import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
-import Rating from "@material-ui/lab/Rating";
+import Rating from "@material-ui/lab";
 
-import mapStyles from "../../mapStyles";
-import useStyles from "./styles.js";
+import useStyles from "./styles";
 
 const Map = () => {
   const classes = useStyles();
